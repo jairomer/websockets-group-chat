e7ef79158a2213c861e6992c856b9d4708eb8950
neusynk <jairomer@protonmail.com>
2021-06-10 23:53:22 +0200

    Remove comments :P

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-/*
-const PORT = 8082;
-const http = require('http').createServer();
-const io = require('socket.io')(http, {
-  cors: {
-    origin: false
-  }
-})
-
-io.on("connection", socket => {
-  console.log('[+] User Connected.');
-
-  // Send message to everyone else.
-  socket.on('message', (message) => {
-    console.log('[+] Received: "'+message+'"');
-    io.emit('message', `${socket.id.substr(0,2)}: ${message}`);
-  });
-});
-
-http.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
-*/
-
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
